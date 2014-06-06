50311bb4e037213c6a71d0fb34dc300cef027f12
Wiktor Mociun <voter101@gmail.com>
2014-06-06 14:13:30 +0200

    Green test

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,14 +2,20 @@ class StringCalculator
   DEFAULT_DELIMETER = /[,\\n]/
 
   def add(expression)
-    extract_numbers(expression).inject(0,:+)
+    numbers = extract_numbers(expression)
+    check_for_negatives(numbers)
+    numbers.inject(0,:+)
   end
 
   private
-
   def extract_numbers(expression)
     _, custom_delimeter, string = expression.match(%r{(\/\/(.{1}))?[\n]?(.*)}).captures
     delimeter = custom_delimeter || DEFAULT_DELIMETER
     string.to_s.split(delimeter).map(&:to_i)
+  end
+
+  def check_for_negatives(numbers)
+    negative = numbers.select { |num| num < 0 }.first
+    raise StandardError.new("negatives not allowed #{negative}") unless negative.nil?
   end
 end
